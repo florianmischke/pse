6b0152c344fc515c21a248ce06e5bc92ec39690d
Florian Mischke <code@florianmischke.de>
2024-02-20 13:26:08 +0100

    format e config and fix css classes

--- app.py
+++ app.py
@@ -1,15 +1,33 @@
 from flask import Flask, render_template, json, jsonify
-import json
+import json, re
 from pprint import pprint
 
 app = Flask(__name__)
+
+def clean_css_classes(str):
+    return str.replace(',', '')
+
+def format_electronconfig(str):
+    regex = r'(\d[spdf])(\d+)'
+    return re.sub(regex, r'\1<sup>\2</sup>', str)
+
+def data_preparation(data):
+    for key in data:
+        if(key == 'order'):
+            continue
+        data[key]['css_classes'] = clean_css_classes(data[key]['category'])
+        data[key]['electron_configuration_formatted'] = format_electronconfig(data[key]['electron_configuration'])
+        data[key]['electron_configuration_semantic_formatted'] = format_electronconfig(data[key]['electron_configuration_semantic'])
+    return data
 
 @app.route("/")
 def home(name=None):
     with open('static/json/periodic-table-lookup.json', 'r', encoding="utf8") as f:
         data = json.load(f)
 
-    return render_template('index.html', name=name, elements=data)
+    elements = data_preparation(data)
+
+    return render_template('index.html', name=name, elements=elements)
 
 
 app.run(host='localhost', debug=True)
