52731c0d39cdbc1b11cff88eae636b901ebe8542
Florian Mischke <code@florianmischke.de>
2024-02-22 17:05:16 +0100

    fixed sizing, scrolling and so on
    mucho responsiveness

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -150,6 +150,7 @@ jQuery(function($) {
     var selector = '#element-'+element_name+' .card'
     $(selector).addClass('focus')
     $(window).scrollTop($(selector).offset().top - $(window).height()/2);
+    $('#main').scrollLeft($(selector).offset().left - $(window).width()/2);
     setTimeout(function() {
       $(selector).removeClass('focus')
     }, 1500)
